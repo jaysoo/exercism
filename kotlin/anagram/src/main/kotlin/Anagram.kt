0a1bc93ce9febde267ea45ee87caece944a7efbf
jackh <jack.hsu@gmail.com>
2016-08-03 16:17:24 -0400

    started elixir

--- kotlin/anagram/src/main/kotlin/Anagram.kt
+++ kotlin/anagram/src/main/kotlin/Anagram.kt
@@ -1,6 +1,6 @@
 data class Anagram(val baseWord: String) {
     private val sortedLowerBaseWord = baseWord.toSortedLowerCase()
-    
+
     fun match(words: List<String>): List<String> =
         words.
             filterNot { it.equals(baseWord, ignoreCase = true) }.
